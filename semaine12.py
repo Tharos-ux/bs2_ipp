442a667fefe8b49e5f3c4cc1172dea926d10e128
tharos <tharos@tcorp.com>
2022-09-26 19:25:24 +0200

    Docstrings week 2
    Les tests unitaires et questions annexes ne sont pas finalisées

--- semaine12.py
+++ semaine12.py
@@ -137,14 +137,36 @@ def read_interaction_file(filename: str) -> Tuple[dict[str, list[str]], list[Tup
 
 
 def count_vertices(filename: str) -> int:
+    """computes the number of unique vertices 
+
+    Args:
+        filename (str): a PPI file
+
+    Returns:
+        int: the number of vertices
+    """
     return len(read_interaction_file_dict(filename).keys())
 
 
 def count_edges(filename: str) -> int:
+    """computes the number of links between summits
+
+    Args:
+        filename (str): a PPI file
+
+    Returns:
+        int: number of edges
+    """
     return len(read_interaction_file_list(filename))
 
 
 def clean_interactome(filein: str, fileout: str) -> None:
+    """Cleans out an interactome file
+
+    Args:
+        filein (str): path to input file
+        fileout (str): path to output file
+    """
     interactions: list[Tuple[str, str]] = read_interaction_file_list(filein)
     with open(fileout, 'w') as handler:
         handler.write('\n'.join(
@@ -152,29 +174,76 @@ def clean_interactome(filein: str, fileout: str) -> None:
 
 
 def get_degree(filename: str, prot: str) -> int:
+    """Gives the number of proteins a specific one is linked to
+
+    Args:
+        filename (str): path to PPI file
+        prot (str): name of given protein
+
+    Returns:
+        int: number of edges connected to prot, if existing, else 0
+    """
     interactions: dict[str, list[str]] = read_interaction_file_dict(filename)
     return len(interactions[prot]) if prot in interactions else 0
 
 
 def get_max_degree(filename: str) -> Tuple[str, int]:
+    """Gives the highest degree of a PPI file
+
+    Args:
+        filename (str): PPI file to scan
+
+    Returns:
+        Tuple[str, int]: protein with max score, and max score
+    """
     mapping: dict[str, int] = {key: len(value)
                                for key, value in read_interaction_file_dict(filename).items()}
     return (list(mapping.keys()).index(max(list(mapping.values()))), max(list(mapping.values())))
 
 
 def get_ave_degree(filename: str) -> int:
+    """Gives the approximate average degree
+
+    Args:
+        filename (str): PPI file
+
+    Returns:
+        int: average degree of PPI interactions, round at unit
+    """
     return int(mean(list({key: len(value)
                           for key, value in read_interaction_file_dict(filename).items()}.values())))
 
 
 def count_degree(filename: str, deg: int) -> list[str]:
+    """Filters some proteins by degree
+
+    Args:
+        filename (str): PPI file
+        deg (int): a degree to check
+
+    Returns:
+        list[str]: all proteins with degree equal to given score
+    """
     return [k for k, v in {key: len(value) for key, value in read_interaction_file_dict(filename).items()}.items() if v == deg]
 
 
-def histogram_degree(filename: str, dmin: int, dmax: int) -> None:
-    return Counter(deg for deg in [len(value) for value in read_interaction_file_dict(filename).values()] if deg >= dmin and deg <= dmax)
-
-
 def output_histogram(data: Counter) -> None:
+    """Plots histogram from counter
+
+    Args:
+        data (Counter): number of number of edges to a node
+    """
     plt.bar(data.keys(), data.values())
     plt.show()
+
+
+def histogram_degree(filename: str, dmin: int, dmax: int) -> None:
+    """Filters some proteins by degree within range
+
+    Args:
+        filename (str): PPI file
+        dmin (int): lower boundary
+        dmax (int): upper boundary
+    """
+    output_histogram(Counter(deg for deg in [len(value) for value in read_interaction_file_dict(
+        filename).values()] if deg >= dmin and deg <= dmax))
